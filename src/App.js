6e0f183f4cd86d5721726feb7223d633b514f9ed
mferradans <mferradanss@gmail.com>
2023-09-14 21:04:04 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Encuestas from './components/Encuestas';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Estado para rastrear las encuestas
+  const encuestas = [
+    { id: 1, pregunta: '¿Cuál es tu color favorito?', opciones: ['Rojo', 'Azul', 'Verde'] },
+    { id: 2, pregunta: '¿Cuál es tu comida favorita?', opciones: ['Pizza', 'Mila', 'Sushi'] },
+  ];
+ return (
+ <div className="App">
+  <h1>Aplicación de Encuestas</h1>
+ {/* Contenido de las páginas */}
+ <Encuestas encuestas={encuestas} />
+ </div>
+ );
 }
 
 export default App;
